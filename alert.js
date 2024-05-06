0ad20396ca43b3a517b2943eb87b7587a2eb0107
Slava <s3990217@gmail.com>
2024-05-06 20:13:07 +0300

    text

--- alert.js
+++ alert.js
@@ -4,11 +4,9 @@ let message;
 
 // копируем значение 'Hello world' из переменной hello в переменную message
 message = hello;
-const alert = (s)=>{
-    console.log(s)
-}
+
 // теперь две переменные содержат одинаковые данные
 alert(hello); // Hello world!
 alert(message); // Hello world!
 
-
+// browser api
